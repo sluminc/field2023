533357373dbb9e841f4a5adb24d645b3ef84d8d8
sluminc <sluminc15@gmail.com>
2023-09-01 16:12:36 -0700

    new projects

--- misc prac/blackjack/script.js
+++ misc prac/blackjack/script.js
@@ -1,6 +1,23 @@
 console.log("Working!");
 
-let firstCard = 5
-let secondCard = 8
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
-console.log(sum);
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+function startGame(params) {
+if (sum<21) {
+    message = "draw a new card?";
+} else if (sum === 21) {
+    message = "hooraaaaay! Blacjack!";
+    hasBlackJack = true
+} else {
+    message = "OH Noo! You are out of the game!";
+    isAlive = false
+}
+
+// cashout
+console.log(message);
+}
---
+++ misc prac/ifs/script.js
@@ -0,0 +1,9 @@
+console.log("Working!");
+
+let age = 22
+
+if (age <21) {
+    console.log("Stay out!!");
+} else {
+    console.log("Welcome to the club");
+}
