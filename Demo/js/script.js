4a983de9ba59fbdf7d1fb1a4d89971096bdcfdec
sluminc <sluminc15@gmail.com>
2023-09-04 09:41:43 -0700

    demo project added

---
+++ Demo/js/script.js
@@ -0,0 +1,33 @@
+// console.log("Working!");
+
+let userError = document.getElementById("user-error")
+let passError = document.getElementById("pass-error")
+let user = document.getElementById("username")
+let pass = document.getElementById("password")
+
+// // userError.textContent = "Username is required"    
+// // passError.textContent = "Password is required"    
+
+// if (user.value === null) {
+//     userError.textContent = "Username is required"   
+// }
+
+// // passError.textContent = "Password is required"
+
+// console.log(userError);
+// console.log(passError);
+
+// function reqInput(params) {
+//     if (!user) {
+//         userError.textContent = "value entered"
+//     }
+
+//     if (!pass) {
+//         passError.textContent = "value entered"
+//     }
+// }
+
+// function validInput(params) {
+//         userError.textContent = "value entered"
+//         passError.textContent = "value entered"    
+// }
